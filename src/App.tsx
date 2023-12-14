a2bb221d132eda6d70a64ac192856a220f8d477b
Ashiqur Rahman <108370520+ashiq-rahman07@users.noreply.github.com>
2023-12-15 03:19:02 +0600

    store setup and counterSlice create

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline ">Tech Net</h1>;
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
+  return (
+    <div className="mt-10 ">
+      <div className="flex gap-3">
+        <button className="mr-5" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <button onClick={() => dispatch(incrementByAmount(5))}>
+          Increment By Amount
+        </button>
+        <div>{count}</div>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
